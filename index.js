420611fb5f81e43522b53b850916c7092f4665cc
tcatsl <thomas.castleman@gmail.com>
2016-11-15 15:50:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ draw2.on("connection", function(socket){
     socket.on("image_recieved", function(){ 
     	setTimeout( function(){
 			for (var thing in drawHistory) 
-			socket.emit("draw_line", { line: drawHistory[thing] } ); console.log("client connected to /draw");
+			socket.emit("draw_line", { line: drawHistory[thing] } );
     
 
 		}, 500);  setTimeout(function(){draw2.emit('chat_message', 'user connected to draw2');}, 3000);
